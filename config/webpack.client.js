68260011a8f9da6d74ef6ea50fda34b06c50d7c7
luoqiang <luoqiang314@gmail.com>
2019-12-14 15:43:07 +0800

    修改配置文件

--- config/webpack.client.js
+++ config/webpack.client.js
@@ -37,10 +37,6 @@ module.exports = {
             loader: 'less-loader',
           },
         ],
-        include: [
-          path.resolve(__dirname, '../src'),
-          path.resolve(__dirname, '../node_modules/antd/')
-        ],
       },
     ],
 },
