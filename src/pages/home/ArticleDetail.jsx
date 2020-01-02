bf53a600d0474cf3ad17ca72b160381d3e41385c
luoqiang <luoqiang314@gmail.com>
2020-01-02 16:54:40 +0800

    文章详情引用markdown组件

--- src/pages/home/ArticleDetail.jsx
+++ src/pages/home/ArticleDetail.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import withStyle, { antdStyle } from '../../withStyle'
-import style from './Home.less'
+import style from './ArticleDetail.less'
 import { getArticleDetail } from '@modules/article'
+import marked from '@components/markdown/helpers/marked'
+import handleCode from '@components/markdown/helpers/handelCode'
+
+
+const getQueryStringArgs = (search) => {
+  const pairs = search.slice(1).split('&')
+  const result = {}
+  pairs.forEach((pair) => {
+    if (pair && pair.indexOf('=') !== -1) {
+      pair = pair.split('=')
+      // 兼容写法
+      result[pair[0]] = result[
+        pair[0].toLocaleLowerCase()
+      ] = decodeURIComponent(pair[1] || '')
+    }
+  })
+
+  return JSON.parse(JSON.stringify(result))
+}
+
 
 @withStyle(style, ...antdStyle('list'))
 @connect(
@@ -30,23 +50,26 @@ class ArticleDetail extends React.Component {
   }
 
   componentDidMount() {
-    // this.getArticleDetail()
-    console.log('history--------', this.props.history)
     const { articleDetail } = this.props
-    console.log('articleDetail------', articleDetail)
+    if (Object.keys(articleDetail).length === 0) {
+      this.getArticleDetail()
+    }
   }
 
   getArticleDetail() {
-    // this.props.getArticleDetail()
+    const { location: { search=''}} = this.props.history
+    const param = getQueryStringArgs(search)
+    this.props.getArticleDetail(param)
   }
 
   render() {
     const { articleDetail: { content_md } } = this.props
 
     return (
-      <div className="home">
-        {content_md}
-      </div>
+      <div
+        className="container for-preview for-markdown-preview"
+        dangerouslySetInnerHTML={{ __html: handleCode(marked(content_md)) }}
+      />
     )
   }
 }
